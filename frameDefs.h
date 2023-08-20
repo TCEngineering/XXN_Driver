76a9f02cb33c76f7d252786c2b73e7e069a61260
TCEngineering <pauldsims4@googlemail.com>
2023-08-20 20:08:12 +0100

    Update frameDefs.h
    Align with arduino-mcp2515/can.h

--- frameDefs.h
+++ frameDefs.h
@@ -1,13 +1,19 @@
 #include <stdint.h>
 
+#define CAN_MAX_DLEN 8
+
+typedef unsigned char __u8;
+typedef unsigned short __u16;
 typedef unsigned long __u32;
+
 typedef __u32 canid_t;
 
-//struct can_frame {
-//    canid_t can_id;  /* 32 bit CAN_ID + EFF/RTR/ERR flags */
-//    uint8_t  can_dlc; /* Data Length Code (number of data bytes)*/
-//    uint8_t  data[8];
-//};
+
+struct can_frame {
+    canid_t can_id;  /* 32 bit CAN_ID + EFF/RTR/ERR flags */
+    __u8    can_dlc; /* frame payload length in byte (0 .. CAN_MAX_DLEN) */
+    __u8    data[CAN_MAX_DLEN];
+};
 
 struct signal_decode {
     uint32_t signal_frame;
