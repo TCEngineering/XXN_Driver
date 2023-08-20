22c533f6d3e554349d38601a1282ed622b8b97fc
TCEngineering <pauldsims4@googlemail.com>
2023-08-20 20:58:46 +0100

    Update frameDefs.h

--- frameDefs.h
+++ frameDefs.h
@@ -10,8 +10,8 @@ typedef __u32 canid_t;
 
 
 struct can_frame {
-    canid_t can_id;  /* 32 bit CAN_ID + EFF/RTR/ERR flags */
-    __u8    can_dlc; /* frame payload length in byte (0 .. CAN_MAX_DLEN) */
+    canid_t can_id;  // 32 bit CAN_ID + EFF/RTR/ERR flags 
+    __u8    can_dlc; // frame payload length in byte (0 .. CAN_MAX_DLEN) 
     __u8    data[CAN_MAX_DLEN];
 };
 
